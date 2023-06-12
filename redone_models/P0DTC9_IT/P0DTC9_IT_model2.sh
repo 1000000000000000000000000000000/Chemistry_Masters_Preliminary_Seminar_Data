c135e72d2928c561469c402615d9a10b77c6d375
1000000000000000000000000000000 <81730040+1000000000000000000000000000000@users.noreply.github.com>
2023-06-12 10:03:42 -0700

    Add files via upload

---
+++ redone_models/P0DTC9_IT/P0DTC9_IT_model2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH -c 8
+#SBATCH --mem=0
+#SBATCH --time=72:00:00
+#SBATCH -D /home/rpearson/research/23ValidationSet/IT/P0DTC9/model_copies
+#SBATCH -J P0DTC9_IT_model2_MQA
+#SBATCH --output=P0DTC9_IT_model2_MQA.out
+
+####### ::: NOTE ::::
+####### To use this enter the following:
+####### >> sbatch <bash script file path> <pdb file name>
+#######
+####### Must be in the directory where the pdb file is!
+
+source activate model_evaluation
+which python
+
+RETURNPATH=$(pwd)
+PDBFILEPATH=$(pwd)/$1
+echo PROCESSING: $PDBFILEPATH
+
+PRETRAINEDMODELPATH='/home/rpearson/tools_bin/model_evaluation_tools/3DCNN_MQA/pretrained_model/pretrained_model.npz'
+
+# Go to directory containing the MQA python file exists
+cd /home/rpearson/tools_bin/model_evaluation_tools/3DCNN_MQA/source
+
+# Run predict_from_pdb.py using CPU Only
+python predict_from_pdb.py -i $PDBFILEPATH -m $PRETRAINEDMODELPATH
+
+cd $RETURNPATH
